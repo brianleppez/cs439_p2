43940b965d851c5c0e79f95adb8873fb3a5d5064
Brian Leppez <brianleppez@gmail.com>
2015-02-19 00:34:03 -0600

    Got the program up and running not sure if it works.

--- threads.c
+++ threads.c
@@ -3,6 +3,7 @@
 #include <setjmp.h>
 #include "threads.h"
 #include <malloc.h>
+#include <stdint.h>
 
 #define CAST(t,e) ((t)(e))
 #define TYPED_MALLOC(t) CAST(t*, malloc(sizeof(t)))
@@ -20,6 +21,7 @@ struct thread {
 
 struct scheduler {
 	unsigned int size;
+	struct thread* current;
 	struct thread* last;
 	struct thread* first;
 	struct thread* next;
@@ -30,6 +32,50 @@ struct scheduler* round_robin;
 int thread_number = 0;
 jmp_buf env;
 uintptr_t sp, bp;
+
+void insert (struct thread* t) {
+	if(round_robin->first == NULL) {
+		round_robin->first = t;
+		t->next = t;
+		round_robin->current = t;
+		round_robin->previous = t;
+		round_robin->last = t;
+		round_robin->size = 1;
+	}
+	else {
+		t->next = round_robin->current;
+		round_robin->previous->next = t;
+		round_robin->previous = t;
+		round_robin->size++;
+	}
+}
+
+void t_remove (struct thread* t) {
+	struct thread* temp = round_robin->first;
+	struct thread* dummy = temp->next;
+	unsigned int count = 0;
+
+	while (dummy != t && count == 0) {
+		temp = dummy;
+		dummy = temp->next;
+		if (temp == round_robin->first)
+			count++;
+	}
+	if (count == 1) {
+		return;
+	}
+	temp->next = dummy->next;
+	round_robin->current = round_robin->current->next;
+	round_robin->previous->next = round_robin->current;
+	free(dummy);
+	round_robin->size -= 1;
+}
+
+void advance() {
+	round_robin->previous = round_robin->current;
+	round_robin->previous->next = round_robin->current->next;
+	printf("current thread is %d\n", round_robin->current->thread_num);
+}
 
 struct thread *thread_create(void (*f)(void *arg), void *arg) {
 	uintptr_t *memptr;
@@ -51,13 +97,13 @@ struct thread *thread_create(void (*f)(void *arg), void *arg) {
 
 void thread_add_runqueue(struct thread *t) {
 	if (round_robin == NULL) {
-		round_robin = TYPED MALLOC(struct scheduler);
+		round_robin = TYPED_MALLOC(struct scheduler);
 		round_robin->first = NULL;
 	}
 	insert(t);
 }
 
-void thread_yield() {
+void thread_yield(void) {
 
 	printf("Thread yielding for thread: %d\n", round_robin->current->thread_num);
 	if(setjmp(round_robin->current->env) == 0) {
@@ -66,101 +112,40 @@ void thread_yield() {
 	}
 }
 
-void dispatch() {
-	if (round_robin->thread_num != 1) {
+void dispatch(void) {
+	if (round_robin->current->thread_num != 1) {
 		__asm__ volatile("mov %%rax, %%rsp" : : "a" (round_robin->previous->esp));
 		__asm__ volatile("mov %%rax, %%rsp" : : "a" (round_robin->previous->ebp));
 	}
 	if (!round_robin->current->dispatched) {
 		round_robin->current->dispatched = 1;
 		printf("Thread %d has been changed to dispatched.\n", round_robin->current->thread_num);
-		round_robin->current->fs(robin->current->args);
+		round_robin->current->fs(round_robin->current->args);
 	}
 	else {
 		__asm__ volatile("mov %%rsp, %%rax" : "=a" (round_robin->current->esp) :); 
 		__asm__ volatile("mov %%rbp, %%rax" : "=a" (round_robin->current->ebp) :);
-		longjmp(robin->current->env,1);
+		longjmp(round_robin->current->env,1);
 	}
 	thread_exit();
 }
 
 
-void schedule() {
+void schedule(void) {
 	advance();
 }
 
-void thread_exit() {
-	remove(round_robin->current);
-	if (robin->size > 0)
+void thread_exit(void) {
+	t_remove(round_robin->current);
+	if (round_robin->size > 0)
 		dispatch();
   	 __asm__ volatile("mov %%rsp, %%rax" : "=a" (sp) :);  
   	 __asm__ volatile("mov %%rbp, %%rax" : "=a" (bp) :);
   	 longjmp(env, 1);
 }
 
-void thread_start_threading() {
+void thread_start_threading(void) {
 	if(setjmp(env) != 0)
 		return;
 	dispatch();
 }
-
-void insert (struct thread* t) {
-	if(round_robin->first == NULL) {
-		round_robin->start = t;
-		t->next = t;
-		round_robin->current = t;
-		round_robin->previous = t;
-		round_robin->last = t;
-		round_robin->size = 1;
-	}
-	else {
-		t->next = round_robin->current;
-		round_robin->previous->next = t;
-		round_robin->previous = t;
-		round_robin->size++;
-	}
-}
-
-void remove (struct thread* t) {
-	struct thread* temp = robin->start;
-	struct thread* dummy = check->next;
-	unsigned int count = 0;
-
-	while (dummy != t && count == 0) {
-		temp = dummy;
-		dummy = temp->next;
-		if (temp == round_robin->start)
-			count++;
-	}
-	if (count == 1) {
-		return;
-	}
-	temp->next = dummy->next;
-	round_robin->current = round_robin->current->next;
-	round_robin->previous->next = round_robin->current;
-	free(dummy);
-	round_robin->size -= 1;
-}
-
-void advance() {
-	round_robin->previous = round_robin->current;
-	round_robin->previous->next = round_robin->current->next;
-	printf("current thread is %d\n", round_robin->current->thread_num);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
